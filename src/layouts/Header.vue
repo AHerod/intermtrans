562ce3e92755b44d01edeb29b056e8ecbce5ebba
Agnieszka Herod <agn.j.herod@gmail.com>
2020-02-09 01:13:49 +0100

    header: add contact-bar

--- src/layouts/Header.vue
+++ src/layouts/Header.vue
@@ -21,6 +21,12 @@
         </div>
       </div>
     </header>
+    <div class="contact-bar">
+      <a href="https://www.facebook.com/Intermtrans-104479624389862/" target="_blank" rel="noopener" class="contact-bar__icon"><g-image alt="Facebook icon" src="~/assets/img/icon_facebook.svg" width="35"/></a>
+      <a href="https://www.linkedin.com/company/intermtrans-damian-talar/about/" class="contact-bar__icon" target="_blank" rel="noopener"><g-image alt="Linkedin icon" src="~/assets/img/icon_linkedin.svg" width="35"/></a>
+      <a href="" class="contact-bar__icon"><g-image alt="Phone icon" src="~/assets/img/icon_phone.svg" width="35"/></a>
+      <a href="" class="contact-bar__icon"><span>+ 48 507 473 964</span></a>
+    </div>
     <slot/>
   </div>
 </template>
@@ -34,7 +40,6 @@
 </static-query>
 
 <style lang="scss">
-
   .header {
     display: flex;
     justify-content: space-between;
@@ -69,7 +74,6 @@
       }
     }
   }
-
   .nav {
     display: flex;
     flex-direction: column;
@@ -137,6 +141,50 @@
           height: 32px;
         }
       }
+    }
+  }
+  .contact-bar {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    background: $cRed;
+    margin-top: -20px;
+    padding: 10px 12px;
+    width: 100%;
+    float: right;
+    position: relative;
+    @include clearfix;
+
+    @media screen and (min-width: 769px) {
+      max-width: 400px;
+    }
+
+    span {
+      color: $cWhite;
+      font-weight: bolder;
+      font-size: large;
+      display: none;
+
+      @media screen and (min-width: 769px) {
+        display: block;
+      }
+    }
+
+    &::before {
+      content: " ";
+      position: absolute;
+      display: block;
+      width: 100%;
+      height: 100%;
+      top: 0;
+      right: 0;
+      z-index: -1;
+      background: $cRed;
+      transform-origin: bottom left;
+      -ms-transform: skew(45deg, 0deg);
+      -webkit-transform: skew(45deg, 0deg);
+      transform: skew(45deg, 0deg);
+      box-shadow: 0px 3px 6px #00000029;
     }
   }
 </style>
