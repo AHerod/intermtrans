fc243d985c2a8a3d59c5486341a9382c6e302931
Agnieszka Herod <agn.j.herod@gmail.com>
2020-05-26 19:40:22 +0200

    fix hero section copy text alignment on small screens

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -3,13 +3,15 @@
     <div class="hero-section__content max-container">
       <h1 class="hero-section__content--title">{{this.lang ? section.eng.titleFirst : section.titleFirst}}</h1>
       <h1 class="hero-section__content--title">{{this.lang ? section.eng.titleSecond : section.titleSecond}}</h1>
-      <h1 :class="[ lang ? 'hero-section__content--title english' : 'hero-section__content--title' ]">{{this.lang ? section.eng.titleThird : section.titleThird}}</h1>
+      <h1 :class="[ lang ? 'hero-section__content--title english' : 'hero-section__content--title' ]">{{this.lang ?
+        section.eng.titleThird : section.titleThird}}</h1>
     </div>
   </section>
 </template>
 
 <script>
-  import section from "~/data/heroSection.json"
+    import section from "~/data/heroSection.json"
+
     export default {
         name: 'HeroSection',
         data() {
@@ -85,7 +87,9 @@
       }
 
       .english {
-        margin-left: 240px;
+        @media screen and (min-width: 992px) {
+          margin-left: 240px;
+        }
       }
     }
   }
